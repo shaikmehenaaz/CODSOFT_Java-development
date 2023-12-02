7b653d0095cca1943c31b1ff30a732daa5f1504b
Mehenaaz.Shaik <94463817+mehenaaz-shaik@users.noreply.github.com>
2023-12-02 11:58:37 +0530

    task 1

--- task1_number_game.java
+++ task1_number_game.java
@@ -1,1 +1,43 @@
+import java.util.Random;
+import java.util.Scanner;
 
+class task1_number_game{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Guess the number game\n");
+        num();
+        while (true){
+            System.out.println("would you like to play again?\nyes or no");
+            String a=sc.nextLine();
+            again(a);
+            if(a.equals("no")){
+                break;
+            }
+        }
+    }
+    static void num(){
+        Scanner sc=new Scanner(System.in);
+        Random rd=new Random();
+        int comp_num= rd.nextInt(1,100);
+        // System.out.println(comp_num);
+        for(int i=0;i<5;i++){
+        System.out.print("Enter your guess: ");
+        int your_num=sc.nextInt();
+        if(comp_num>your_num){
+            System.out.println("lower than the actual\n");
+        }else if(comp_num<your_num){
+            System.out.println("higher than the actual\n");
+        }else{
+            System.out.println("correct guess\n");
+            break;
+        }
+        }
+    }
+    static void again(String a){
+        if(a.equals("yes")){
+            num();
+        }else{
+            System.out.println("good work");
+        }
+    }
+}
